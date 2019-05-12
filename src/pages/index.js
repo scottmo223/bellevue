31285cb816cd69d47a92c8088fef29f7117a260c
Scott Morales <morales.scott@gmail.com>
2019-05-12 00:18:34 -0500

    Update content some more

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,37 +39,29 @@ class Index extends React.Component {
           }}>Pricing</div></h1>
           <div>
             <h4>Wedding</h4>
-            <p>$2500 per event for up to 180 people.</p>
-            <p>Included:
-Tables and chairs
-Ice
-Use of the bar area
-Bride and groom’s rooms
-Use of the back porch and grounds
-You will have exclusive use of the venue for twelve hours. Please note that while our white garden chairs are included in this package, set up and tear down is the responsibility of the customer. Belle Vue will provide this service upon request.
-The following additions are available upon request:
-
-Full dinner service including round ivory dinner plates, round ivory cake plates, 16 oz water goblets, and silverware. This includes setting the table, clearing away, and cleaning. $500.
-
-Bartender services - we will hire a TABC certified bartender to serve your guests for up to four hours. All alcohol, cups, and bar napkins are to be provided by the customer. $200.
-
-While the use of our outdoor garden chairs are free, customers are responsible to set them up as they desire, and return them to their original position. Belle Vue will provide this service for $300.
-
-Facility cleaning charge: no fee if before the 12 hour usage time is up, the customer removes all items they brought, puts chairs on tables, sweeps, cleans rooms and bathrooms and takes out all trash to dumpster provided on the property. Cleaning can be done by Belle Vue staff for a $200 fee.
-
-Any required time before the 12 hour time frame or after has a charge of $150 an hour.
-
-PARTY PRICING-
-
-Party pricing $750: includes tables, chairs, hall area, bar area and back porch. 6 hours of usage time, 1 additional hour for clean up. Clean up is required by customer which includes no fee. 1 hour is given for cleaning time where the customer removes all items they brought, puts chairs on tables, sweeps, cleans rooms and bathrooms and takes out all trash to dumpster provided on the property. Cleaning can be done by Belle Vue staff for a $200 fee.
-
-Any required time before the 6 hour time frame or after has a charge of $150 an hour.
-
-</p>
-
+            <h5>$2500 per event for up to 180 people.</h5>
+            <ul>Included:
+              <li>Tables and chairs</li>
+              <li>Ice</li>
+              <li>Use of the bar area</li>
+              <li>Bride and groom’s rooms</li>
+              <li>Use of the back porch and grounds</li>
+            </ul>
+            <p>You will have exclusive use of the venue for twelve hours. Please note that while our white garden chairs are included in this package, set up and tear down is the responsibility of the customer. Belle Vue will provide this service upon request.</p>
+            <p>The following additions are available upon request:</p>
+            <p>Full dinner service including round ivory dinner plates, round ivory cake plates, 16 oz water goblets, and silverware. This includes setting the table, clearing away, and cleaning. $500.</p>
+            <p>Bartender services - we will hire a TABC certified bartender to serve your guests for up to four hours. All alcohol, cups, and bar napkins are to be provided by the customer. $200.</p>
+            <p>While the use of our outdoor garden chairs are free, customers are responsible to set them up as they desire, and return them to their original position. Belle Vue will provide this service for $300.</p>
+            <p>Facility cleaning charge: no fee if before the 12 hour usage time is up, the customer removes all items they brought, puts chairs on tables, sweeps, cleans rooms and bathrooms and takes out all trash to dumpster provided on the property. Cleaning can be done by Belle Vue staff for a $200 fee.</p>
+            <p>Any required time before the 12 hour time frame or after has a charge of $150 an hour.</p>
+          </div>
+          <div>
+            <h4>Party Pricing</h4>
+            <h5>Party pricing $750</h5>
+            <p>Includes tables, chairs, hall area, bar area and back porch. 6 hours of usage time, 1 additional hour for clean up. Clean up is required by customer which includes no fee. 1 hour is given for cleaning time where the customer removes all items they brought, puts chairs on tables, sweeps, cleans rooms and bathrooms and takes out all trash to dumpster provided on the property. Cleaning can be done by Belle Vue staff for a $200 fee.</p>
+            <p>Any required time before the 6 hour time frame or after has a charge of $150 an hour.</p>
           </div>
         </div>
-
       </Layout>
     )
   }
