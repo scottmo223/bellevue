35b08c8be37d94ba113984379d150a685f16f523
Scott Morales <morales.scott@gmail.com>
2019-05-14 04:38:47 -0500

    change photos

--- src/components/pictures.js
+++ src/components/pictures.js
@@ -2,29 +2,24 @@ import React from 'react'
 import ImageGallery from "react-image-gallery"
 
 import "react-image-gallery/styles/css/image-gallery.css"
+import image1 from "../../content/assets/photo2.jpg"
+import image2 from "../../content/assets/photo3.jpg"
+import image3 from "../../content/assets/photo4.jpg"
 
 class Pictures extends React.Component{
     render() {
         const images = [
             {
-                original: "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-                thumbnail: "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                original: image1,
+                thumbnail: image1
             },
             {
-                original: "https://images.unsplash.com/photo-1521543832500-49e69fb2bea2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-                thumbnail: "https://images.unsplash.com/photo-1521543832500-49e69fb2bea2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                original: image2,
+                thumbnail: image2
             },
             {
-                original: "https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-                thumbnail: "https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-            },
-            {
-                original: "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-                thumbnail: "https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-            },
-            {
-                original: "https://images.unsplash.com/photo-1541250848049-b4f7141dca3f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
-                thumbnail: "https://images.unsplash.com/photo-1541250848049-b4f7141dca3f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80"
+                original: image3,
+                thumbnail: image3
             },
         ]
 
@@ -36,6 +31,7 @@ class Pictures extends React.Component{
                 useBrowserFullscreen={false}
                 showBullets={true}
                 slideDuration={1000}
+                
             />
         )
     }
